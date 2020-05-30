94a5447429b36ad873dba60c56ab798a67822e10
asabogal <asabogal.git@gmail.com>
2020-05-29 22:28:52 -0400

    import Poppins fonts

--- src/index.js
+++ src/index.js
@@ -1,6 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import './index.css';
+// fonts
+import './fonts/Poppins-Black.ttf';
+import './fonts/Poppins-BlackItalic.ttf';
+import './fonts/Poppins-Bold.ttf';
+import './fonts/Poppins-BoldItalic.ttf';
+import './fonts/Poppins-ExtraBold.ttf';
+import './fonts/Poppins-ExtraBoldItalic.ttf';
+import './fonts/Poppins-ExtraLight.ttf';
+import './fonts/Poppins-ExtraLightItalic.ttf';
+import './fonts/Poppins-Italic.ttf';
+import './fonts/Poppins-Light.ttf';
+import './fonts/Poppins-LightItalic.ttf';
+import './fonts/Poppins-Medium.ttf';
+import './fonts/Poppins-MediumItalic.ttf';
+import './fonts/Poppins-Regular.ttf';
+import './fonts/Poppins-SemiBold.ttf';
+import './fonts/Poppins-SemiBoldItalic.ttf';
+import './fonts/Poppins-Thin.ttf';
+import './fonts/Poppins-ThinItalic.ttf';
 
 ReactDOM.render(
   <React.StrictMode>
