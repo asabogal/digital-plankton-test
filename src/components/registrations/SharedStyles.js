4cffdc414083c85e503ee79f65c58fd3d3354b10
asabogal <asabogal.git@gmail.com>
2020-06-01 04:06:55 -0400

    styled top form + add label animations

--- src/components/registrations/SharedStyles.js
+++ src/components/registrations/SharedStyles.js
@@ -1,43 +1,67 @@
 import styled from 'styled-components';
 
 export const FormsContainer = styled.div`
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  width: 30vw;
-  margin: 0 auto;
+  justify-self: center;
+  align-self: center;
+  background-color: rgba(0,0,0,0.73);
+  width: 325px;
+  height: 560px;
+  padding: 2rem 4rem;
+  z-index: 2;
 `;
 
 export const Legend = styled.legend`
   font-family: 'Poppins-SemiBold';
-  font-size: 32px;
+  font-size: 30px;
   width: 100%;
+  margin: 2rem 0;
 `;
 
 export const Form = styled.form`
-  display: grid;
-  grid-template-columns: 80%;
-  grid-template-rows: repeat(4, auto);
-  justify-content: center;
-`;
 
-export const Label = styled.label`
-  font-size: 0.8em;
-  font-family: 'Poppins-Light';
 `;
 
 export const InputWrapper = styled.div`  
+  display: flex;
+  margin-bottom: 0.8rem;
+  position: relative;
+  span {
+    position: absolute;
+    right: 1rem;
+    top: 1rem;
+    color: lightgrey;
+    font-size: 0.8em;
+  }
+`;
+
+export const Label = styled.label`
+  font-size: 0.9em;
+  font-family: 'Poppins-Light';
+  position: absolute;
+  top: 1.1rem;
+  left: 1rem;
+  color: lightgrey;
+  transition: font-size 0.2s ease, top 0.2s ease;
 `;
 
 export const Input = styled.input`
-  border: 0px thin black;
-  height: 20px;
   margin-bottom: 20px;
+  padding: 1.3rem 1rem 0.7rem 1rem;
+  flex: 1;
+  background-color: #333;
+  border: none;
+  border-radius: 5px;
+  color: white;
+  &:focus {
+    background-color: #454545;
+    + label {
+      font-size: 0.5rem;
+      top: 0.3rem;
+    }
+  }
 `;
 
 export const Submit = styled.div`
-
 `;
 
 export const Options = styled.div`
@@ -47,5 +71,5 @@ export const RememberMe = styled.div`
 `;
 
 export const SignUpContent = styled.div`
-  
+
 `;
