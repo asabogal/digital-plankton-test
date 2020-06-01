4564ee4e8331be771abc76b6b54cde8063410bc1
asabogal <asabogal.git@gmail.com>
2020-06-01 17:04:26 -0400

    init SignUp form

---
+++ src/components/registrations/SignUp.js
@@ -0,0 +1,86 @@
+import React, {useReducer} from 'react';
+// styles
+import {FormsContainer, Form, Legend, InputWrapper, Label, Input, Submit, Options, RememberMe, SignUpContent, Footer} from './SharedStyles';
+
+const SignUp = () => {
+
+  const [userInput, setUserInput] = useReducer((state, newState) => ({...state, ...newState}),
+    {
+      email: '',
+      password: ''
+    }  
+  );
+
+  const handleChange = (e) => {
+    const {name, value} = e.target;
+    setUserInput({
+      [name]: value
+    });
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert('form submitted');
+  }
+
+  return (
+    <FormsContainer>
+      <Form onSubmit={handleSubmit}>
+        <Legend>Sign Up</Legend>
+        <InputWrapper>
+          <Input
+            type='text'
+            name='name'
+            value={userInput.value}
+            onChange={handleChange}
+          />
+          <Label>Name</Label>
+        </InputWrapper>
+        <InputWrapper>
+          <Input
+            type='email'
+            name='email'
+            value={userInput.value}
+            onChange={handleChange}
+          />
+          <Label>Email</Label>
+        </InputWrapper>
+        <InputWrapper>
+          <Input
+            type='password'
+            name='password'
+            value={userInput.value}
+            onChange={handleChange}
+          />
+          <Label>Password</Label>
+          <span>SHOW</span>
+        </InputWrapper>
+        <Submit>
+          <Input
+            type='submit'
+            value='Sign Up'
+          />
+        </Submit>
+        </Form>
+        {/* <Options>
+          <RememberMe>
+            <input
+              type='checkbox'
+            />
+            <label>Remember Me</label>
+          </RememberMe>
+          <a href='#'>Need Help?</a>
+        </Options> */}
+        <SignUpContent>
+          <h4>Already Signed up?</h4>
+          <span><p>Log in now.</p></span>
+        </SignUpContent>
+        <Footer>
+          <p>This form and the contents of this page are for ilustrational purposes only.</p>
+        </Footer>
+    </FormsContainer>
+  );
+};
+
+export default SignUp;
+
