cb8c0573baca2b85cbb629f8c41967b1cca684e6
asabogal <asabogal.git@gmail.com>
2020-06-01 01:29:18 -0400

    init Register component with background image

---
+++ src/components/registrations/Register.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import styled from 'styled-components';
+import image from '../../images/netflix_background.png';
+
+const Register = () => {
+  return (
+    <PageContainer>
+      
+    </PageContainer>
+  );
+};
+
+export default Register;
+
+const PageContainer = styled.div`
+  width: 100%;
+  height: 100vh;
+  position: relative;
+  background: url(${image}) no-repeat center center/cover;
+`;
