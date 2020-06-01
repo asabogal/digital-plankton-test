9a55fd85eb78def3b023d72e2b2ea684f26b1102
asabogal <asabogal.git@gmail.com>
2020-05-31 21:45:25 -0400

    init router with registrations

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+// components
+import SignIn from './components/registrations/SignIn';
+import SignUp from './components/registrations/SignUp';
+
 
 const App = () => {
   return (
-    <div>
-      DIGITAL PLANKTON
-    </div>
+    <>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path='/signin' component={SignIn}/>
+          <Route exact path='/signup' component={SignUp}/>
+        </Switch>
+      </BrowserRouter>
+    </>
   );
 }
 
