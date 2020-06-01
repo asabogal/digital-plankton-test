e2854e92e8742bea4d3cc08bdac2924bdfebe6ff
asabogal <asabogal.git@gmail.com>
2020-06-01 04:03:18 -0400

    add opacity to background image with pseudo element

--- src/components/registrations/Register.js
+++ src/components/registrations/Register.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import styled from 'styled-components';
+import SignIn from './SignIn';
 import image from '../../images/netflix_background.png';
 
 const Register = () => {
   return (
     <PageContainer>
-      
+      <SignIn/>
     </PageContainer>
   );
 };
@@ -13,8 +14,19 @@ const Register = () => {
 export default Register;
 
 const PageContainer = styled.div`
+  display: grid;
   width: 100%;
   height: 100vh;
   position: relative;
   background: url(${image}) no-repeat center center/cover;
+  &::after {
+  content: '';
+	position: absolute;
+	top: 0;
+	left: 0;
+	width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  z-index: 1;
+  }
 `;
