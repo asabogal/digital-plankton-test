bc9202768942a76715d92277cf4995a8ab792b68
asabogal <asabogal.git@gmail.com>
2020-06-02 19:16:30 -0400

    add buttons util

---
+++ src/components/utils/Buttons.js
@@ -0,0 +1,33 @@
+import styled from 'styled-components';
+
+export const PlayButton = styled.button`
+  background-color: white;
+  color: black;
+  width: 110px;
+  height: 42px;
+  border-radius: 5px;
+  font-family: 'Poppins-SemiBold';
+  font-size: 1rem;
+  cursor: pointer;
+  &:hover {
+    background-color: #e0dede;
+  }
+  &::before {
+    content: '\u25B6';
+    padding-right: 10px;
+  }
+`;
+
+export const InfoButton = styled(PlayButton)`
+  background-color: rgba(141, 141, 141, 0.5);
+  color: white;
+  width: 125px;
+  &:hover {
+    background-color: rgba(88, 88, 88, 0.5);
+  }
+  &::before {
+    content: '\u2139';
+    padding-right: 10px;
+    font-size: 1.2rem;
+  }
+`;
