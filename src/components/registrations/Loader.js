435e95e71c87c76666f45896a7d6b0fcef30ee0c
asabogal <asabogal.git@gmail.com>
2020-06-02 12:49:21 -0400

    increase spinner boarder

--- src/components/registrations/Loader.js
+++ src/components/registrations/Loader.js
@@ -14,14 +14,15 @@ export default Loader;
 const Spinner = styled.div`
   width: 70px;
   height: 70px;
-  border: 5px solid #e50914;
-  border-left: 5px solid rgba(255, 255, 255, 0.2);
+  border: 10px solid #e50914;
+  border-left: 10px solid rgba(255, 255, 255, 0.2);
   border-radius: 100%;
   justify-self: center;
   align-self: center;
   animation: spin 0.6s infinite linear;
   position: absolute;
   top: 45%;
+  z-index: 3;
   @keyframes spin {
     from {
       transform: rotate(0deg);
