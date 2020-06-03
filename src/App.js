14d157532f5360501d3f2c524cd4bd1bd40a9261
asabogal <asabogal.git@gmail.com>
2020-06-03 01:18:03 -0400

    add Login and Logout logic + protect routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,37 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
 // components
 import Register from './components/registrations/Register';
-
+import Profile from './components/main/Profile';
+import ProtectedRoute from './components/routing/ProtectedRoute';
 
 const App = () => {
+
+  const [loggedIn, setLoggedIn] = useState(false);
+  
+  const handleLogin = (user) => {
+    setLoggedIn((true));
+  }
+
+  const handleLogout = () => {
+    setLoggedIn((false));
+  }
+
   return (
     <>
       <BrowserRouter>
         <Switch>
-          <Route exact path='/' component={Register}/>
+          <Route 
+            exact path='/register' 
+            render={props => (
+            <Register {...props} handleLogin={handleLogin}/>
+            )}
+          />
+          <ProtectedRoute 
+            exact path='/profile' 
+            component = {Profile}
+            loggedIn={loggedIn}
+          />
         </Switch>
       </BrowserRouter>
     </>
